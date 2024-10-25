0272678c8fb31a154e7613729cc90dce83b1a6e1
Annisa Zahra <87129655+annisazahra01@users.noreply.github.com>
2024-10-25 14:40:28 +0700

    Update app_SEKDA.py

--- app_SEKDA.py
+++ app_SEKDA.py
@@ -129,7 +129,7 @@ def main():
     #data = response.json()
 
 
-    file_path = "https://univindonesia-my.sharepoint.com/personal/annisa_zahra01_office_ui_ac_id/_layouts/15/download.aspx?share=EZXVNPNPIZ9PjG_P-11IhikBXkyWxJ-SJe-8Vnv1TWmCfg"
+    file_path = "https://univindonesia-my.sharepoint.com/personal/annisa_zahra01_office_ui_ac_id/_layouts/15/download.aspx?share=EZ4eO2Fc6u1Lpb1urKG7x9ABV9bJaZeMZGAky4ZHDl32Ag"
     response = requests.get(file_path)
     data = response.json()
 
