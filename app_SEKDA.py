ba6b23c0cba3c5753f261579747c8b84ca7512ab
Annisa Zahra <87129655+annisazahra01@users.noreply.github.com>
2024-11-05 09:28:11 +0700

    Update app_SEKDA.py

--- app_SEKDA.py
+++ app_SEKDA.py
@@ -300,6 +300,36 @@ def main():
     with col1:
         st.markdown("<h4 style='text-align: left;'>Apa yang ingin dilakukan?</h4>", unsafe_allow_html=True)
 
+        if st.button("Lihat Hasil Cek Vertikal Keseluruhan"):
+            for i in range(len(clean_data)):
+                df_clean = pd.DataFrame(clean_data[clean_keys_list[i]])
+                if df_clean is not None and not df_clean.empty and not (len(df_clean.columns) == 2 and 'Keterangan' in df_clean.columns):
+                    df_summary = pd.DataFrame(summary_data[sum_keys_list[i]])
+                    kode_provinsi, tabel = clean_keys_list[i].split('-')
+                    nama_provinsi = provinsi_mapping.get(kode_provinsi, ['Unknown'])[0]
+                    inew = f"{nama_provinsi} ({kode_provinsi}) - Tabel {tabel}"
+
+                    st.markdown(divider_style, unsafe_allow_html=True)
+                    st.subheader(f"{inew}")
+
+                    display_dataframe(df_summary)
+                    st.markdown('**Keterangan**')
+                    st.text('✓: Data sudah konsisten pada periode tersebut')
+
+                    with st.expander("Lihat Detail"):
+                        st.write("""
+                        **Penjelasan Warna:**
+                        - 🟩 : Aggregat
+                        - 🟨 : Calculated
+                        - 🟥 : Selisih
+                        """)
+                        st.dataframe(df_clean.style.apply(lambda row: highlight_rows(row, df_clean),axis=1)
+                        .set_properties(**{'text-align': 'center'})  # Set text alignment to center
+                        .set_table_styles([  # Apply styling to the header
+                        {'selector': 'th', 'props': [('text-align', 'center'), ('background-color', '#E8F6F3')]}])
+                        .format(precision=2)  # Format numerical values with two decimal places
+                        )
+
         # Create a button for each distinct number, replace number with province name
         for num in distinct_numbers:
             # Get the province name from the mapping, default to num if not found
