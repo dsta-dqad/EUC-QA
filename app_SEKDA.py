1396bc7823505b7d5aacfb71fb9b33d41b0938fd
Annisa Zahra <87129655+annisazahra01@users.noreply.github.com>
2024-11-05 14:36:37 +0700

    Update app_SEKDA.py

--- app_SEKDA.py
+++ app_SEKDA.py
@@ -319,6 +319,8 @@ def main():
         st.session_state.show_all_results_hori = False
     if "show_all_results_beforeafter" not in st.session_state:
         st.session_state.show_all_results_beforeafter = False
+    if "selected_table" not in st.session_state:
+        st.session_state.selected_table = None
 
     with col1:
         st.markdown("<h4 style='text-align: left;'>Apa yang ingin dilakukan?</h4>", unsafe_allow_html=True)
@@ -327,16 +329,19 @@ def main():
             st.session_state.show_all_results_verti = True
             st.session_state.show_all_results_hori = False
             st.session_state.show_all_results_beforeafter = False
+            st.session_state.selected_table = None
 
         if st.button("Lihat Hasil Horizontal Check Keseluruhan"):
             st.session_state.show_all_results_verti = False
             st.session_state.show_all_results_hori = True
             st.session_state.show_all_results_beforeafter = False
+            st.session_state.selected_table = None
 
         if st.button("Lihat Hasil Before After Check Keseluruhan"):
             st.session_state.show_all_results_verti = False
             st.session_state.show_all_results_hori = False
             st.session_state.show_all_results_beforeafter = True
+            st.session_state.selected_table = None
 
         # Create a button for each distinct number, replace number with province name
         for num in distinct_numbers:
@@ -352,6 +357,9 @@ def main():
 
                     # Use a unique key for each button by appending the full table name
                     if st.button(f"Lihat Tabel {table_label}", key=f"button_{table}"):
+                        st.session_state.show_all_results_verti = False
+                        st.session_state.show_all_results_hori = False
+                        st.session_state.show_all_results_beforeafter = False
                         st.session_state.selected_table = table
 
 
